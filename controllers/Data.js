c313356ec88b62a228a8a84bb83c743a33448bab
Kuldeep Rathod <kuldeeprathod33@gmail.com>
2022-09-05 01:05:48 +0530

    imp

---
+++ controllers/Data.js
@@ -0,0 +1,109 @@
+const get  = (req,res)=>{
+    res.send()
+
+    [{
+        id:"1",
+        brand:"Apple",
+        name:"Apple iphone 13 Pro",
+        img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone13pro-202203?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1644989935267",
+        price:800
+    },
+    {
+        id:"2",
+        brand:"Apple",
+        name:"Apple iphone 12",
+        img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone12purple-202109?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1646335268265",
+        price:700
+    },
+        
+        {
+            id:"3",
+            brand:"Apple",
+            name:"Apple AirPods",
+            img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73?wid=445&hei=445&fmt=jpeg&qlt=95&.v=1632861342000",
+            price:300
+        },
+        {
+            id:"4",
+            brand:"Apple",
+            name:"Apple iWatch",
+            img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MKUU3_VW_34FR+watch-44-alum-gold-nc-se_VW_34FR_WF_CO_GEO_IN?wid=375&hei=356&trim=1%2C0&fmt=p-jpg&qlt=95&.v=1632171096000%2C1630708161000",
+            price:459
+        },
+        {
+            id:"5",
+            brand:"Apple",
+            name:"Apple MacBook Air",
+            img :"https://www.apple.com/v/macbook-air-m2/b/images/overview/design/design_top_midnight__codbsd86bjxy_large.jpg",
+            price:850
+        },
+        
+        
+        {
+            id:"6",
+            brand:"Apple",
+            name:"Apple Airpods Max",
+            img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-select-green-202011?wid=445&hei=445&fmt=jpeg&qlt=95&.v=1604022364000",
+            price:480
+        },
+        
+        {
+            id:"7",
+            brand:"Apple",
+            name:"Apple iphone lightning dock",
+            img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MQHX2?wid=445&hei=445&fmt=jpeg&qlt=95&.v=1504288101848",
+            price:200
+        },
+        
+        {
+            id:"8",
+            brand:"Apple",
+            name:"Apple HomePod mini",
+            img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/homepod-mini-select-blue-202110?wid=445&hei=445&fmt=jpeg&qlt=95&.v=1632925511000",
+            price:239
+        },
+        {
+            id:"9",
+            brand:"Apple",
+            name:"Apple MagSafe",
+            img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MJWY3?wid=445&hei=445&fmt=jpeg&qlt=95&.v=1625613219000",
+            price:199
+        },
+        {
+            id:"10",
+            brand:"Apple",
+            name:"Apple iphone SE",
+            img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphonese-202203?wid=340&hei=264&fmt=p-jpg&qlt=95&.v=1646415838921",
+            price:399
+        },
+        {
+            id:"11",
+            brand:"Apple",
+            name:"Apple MacBook 13",
+            img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202206?wid=452&hei=420&fmt=jpeg&qlt=95&.v=1653493200207",
+            price:799
+        },
+        {
+            id:"12",
+            brand:"Apple",
+            name:"Apple SmartWatch",
+            img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MKUN3_VW_34FR+watch-45-alum-green-nc-7s_VW_34FR_WF_CO?wid=375&hei=356&trim=1%2C0&fmt=p-jpg&qlt=95&.v=1632171062000%2C1631661629000",
+            price:350
+        },
+        {
+            id:"13",
+            brand:"Apple",
+            name:"Apple AirPods 2nd gen",
+            img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MV7N2?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1551489688005",
+            price:230
+        },
+        {
+            id:"14",
+            brand:"Apple",
+            name:"Apple AirPods Pro",
+            img :"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MWP22?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1591634795000",
+            price:349
+        }        
+    ]
+}
+    module.exports.ApiController = get
